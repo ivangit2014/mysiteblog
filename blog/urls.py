97b3bee7cf91bb4b57583e71d3d979df22b99882
ivan <ivanrelos@gmail.com>
2016-12-02 19:47:23 -0400

    manejo de formularios

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, post_new, post_edit
 
 urlpatterns=[
 	url(r'^listar',post_list, name='post_listar'),
 	url(r'^(?P<id_post>[0-9]+)/$',post_detail, name='post_detalle'),
+	url(r'^new',post_new, name='post_crear'),
+	url(r'^edit/(?P<id_post>[0-9]+)/$',post_edit, name='post_editar'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from blog.models import Post
+from blog.forms import PostForm
+
 
 # Create your views here.
 def post_list(request):
@@ -12,3 +15,26 @@ def post_detail(request, id_post):
 	contexto = {'post':post}
 	return render(request, 'blog/post_detail.html', contexto)
 
+def post_new(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+		return redirect('blog:post_detalle', id_post=post.id)
+	else:
+		form = PostForm()
+		return render(request,'blog/post_form.html',{'form':form})
+
+def post_edit(request,id_post):
+	post = Post.objects.get(id=id_post)
+	if request.method == 'GET':
+		form = PostForm(instance = post)
+	else:
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			form.save()
+		return redirect('blog:post_detalle', id_post=post.id)
+	return render(request, 'blog/post_form.html',{'form':form})
