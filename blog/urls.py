66005b39348e84b9f184eba493f9a4a1ac0c40ec
ivan <ivanrelos@gmail.com>
2016-12-01 21:03:52 -0400

    vista url enlace para post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from blog.views import post_list
+from blog.views import post_list, post_detail
 
 urlpatterns=[
 	url(r'^listar',post_list, name='post_listar'),
+	url(r'^(?P<id_post>[0-9]+)/$',post_detail, name='post_detalle'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,9 @@ def post_list(request):
 	post = Post.objects.all()
 	contexto = {'posts':post}
 	return render (request, 'blog/post_list.html',contexto)
+
+def post_detail(request, id_post):
+	post = Post.objects.get(id=id_post)
+	contexto = {'post':post}
+	return render(request, 'blog/post_detail.html', contexto)
+
